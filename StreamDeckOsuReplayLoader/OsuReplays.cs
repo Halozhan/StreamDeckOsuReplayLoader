d3be6fa23b2ca633bfd9c6df409adf918848f42a
Halozhan <juwonhwang0411@gmail.com>
2024-07-16 00:46:44 +0900

    Separate the files into KeyMacro.cs and OsuReplay.cs and implement a path that can be specified in PropertyInspector.

---
+++ StreamDeckOsuReplayLoader/OsuReplays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace StreamDeckOsuReplayLoader
+{
+    internal class OsuReplays
+    {
+        public static void LoadRecentReplay(String replayPath)
+        {
+            // ref: http://www.gisdeveloper.co.kr/?p=2044
+            //string replaypath = "F:\\osu!\\Replays\\";
+            DirectoryInfo di = new DirectoryInfo(replayPath);
+            string LatestReplay = string.Empty;
+            DateTime LatestReplayTime = new DateTime();
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (file.Extension.ToLower().CompareTo(".osr") == 0)
+                {
+                    if (LatestReplayTime <= file.LastWriteTimeUtc)
+                    {
+                        LatestReplay = file.FullName;
+                        LatestReplayTime = file.LastWriteTimeUtc;
+                    }
+                }
+            }
+            Process LatestOsr = new Process();
+            LatestOsr.StartInfo.FileName = LatestReplay;
+            LatestOsr.Start();
+        }
+    }
+}
